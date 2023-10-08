ba83ca1c24b480dbdf507dfbcc5baf7197264b2d
DanilNaum <90374685+DanilNaum@users.noreply.github.com>
2023-10-08 23:24:34 +0300

    Реализована базовая функциональность, заложеная проектом

--- cmd/task_scheduler/main.go
+++ cmd/task_scheduler/main.go
@@ -3,56 +3,131 @@ package main
 import (
 	"DanilNaum/task_scheduler/internal/testtasks"
 	"bufio"
-	"fmt"
+	"time"
 	"os"
-	"sync"
 	"strings"
+	"sync"
 )
 
-
-
 func main() {
-	dir, err := os.Getwd()
-    if err != nil {
-        panic(err)
-    }
-	fmt.Print(dir)
-	file, err := os.Open(dir + "/cmd/task_scheduler/tasks.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	s := bufio.NewScanner(file)
+	var second_task []string
+	var minute_task []string
+	var hour_task []string
+	mu := new(sync.Mutex)
 	wg := new(sync.WaitGroup)
-	numberOfSimultaneousRequests := 2
-	channel := make(chan string)
+	numberOfSimultaneousRequests := 5
+	data_channel := make(chan string)
+	go func(){
+		defer close(data_channel)
+		for{
+			dir, err := os.Getwd()
+			if err != nil {
+				panic(err)
+			}
+			file, err := os.Open(dir + "/cmd/task_scheduler/tasks.txt")
+			if err != nil {
+				panic(err)
+			}
+			
+			s := bufio.NewScanner(file)
+			var second_task_tmp []string
+			var minute_task_tmp []string
+			var hour_task_tmp []string
+			for s.Scan(){
+				if strings.Split(s.Text()," ")[0] == "secondly"{
+					second_task_tmp = append(second_task_tmp, s.Text())
+				}else if strings.Split(s.Text()," ")[0] == "minutely"{
+					minute_task_tmp = append(minute_task_tmp, s.Text())
+				}else if strings.Split(s.Text()," ")[0] == "hourly"{
+					hour_task_tmp = append(hour_task_tmp, s.Text())
+				}
+			}
+			mu.Lock()
+			second_task = second_task_tmp
+			minute_task = minute_task_tmp
+			hour_task = hour_task_tmp
+			mu.Unlock()
+			file.Close()
+		}
+	}()
+	go func() {
+		
+		tickSecond := time.NewTicker(time.Second)
+		tickMinute := time.NewTicker(time.Minute)
+		tickHour := time.NewTicker(time.Hour)
+		defer tickSecond.Stop() // освободим ресурсы, при завершении работы функции
+		defer tickMinute.Stop()
+		defer tickHour.Stop()
+		for {
+			select {
+				case <-tickSecond.C:
+					mu.Lock()
+					for _,task :=range (second_task){
+						data_channel<-task
+					}
+					mu.Unlock()
+				case <-tickMinute.C:
+					mu.Lock()
+					for _,task :=range (minute_task){
+						data_channel<-task
+					}
+					mu.Unlock()
+				case <-tickHour.C:
+					mu.Lock()
+					for _,task :=range (hour_task){
+						data_channel<-task
+					}
+					mu.Unlock()
+			}
+		}
+	}()
+
+	channel := make(chan []string)
 	channel2 := make(chan struct{}, numberOfSimultaneousRequests)
 	
 	go func(){
 		defer close(channel)
 		defer close(channel2)
-		for s.Scan(){
+		for s := range data_channel{
+			a := strings.Split(s," ")
 			channel2 <- struct{}{} // канал для проверки завершения выполнения
-			//  предыдущей задачи и ограничения количества одновреммено выполняемых задач
-			channel <- s.Text()	// канал для передачи следующей задачи в фукцию
+				//  предыдущей задачи и ограничения количества одновреммено выполняемых задач
+			channel <- a	// канал для передачи следующей задачи в фукцию
 		}
-
-		// close(channel)
-		// close(channel2)
 	}()
-	for t := range channel{
-		// t :=s.Text()
-		a := strings.Split(t," ")
-		// fmt.Println(a)
-		// a := []string{"abc", "1", "5"}
+	
+	// Наличие структуры на thread означает, что он свободен и можно передать на него задачу
+	thread := make(map[int] chan struct{})
+	for i := 0; i < 5; i++ {
+		thread[i] = make(chan struct{},1)
+		thread[i] <-  struct{}{}
+		defer close(thread[i])
+	}
+		
+	for a := range channel{
 		wg.Add(1)
-		go func(wg *sync.WaitGroup){
+		go func(wg *sync.WaitGroup, a []string){
 			defer wg.Done()
-			testtasks.Wait(a[2:])
+			select {
+			case <-thread[0]:
+				testtasks.Wait(1,a[2:])
+				thread[0] <-  struct{}{}
+			case <-thread[1]:
+				testtasks.Wait(2,a[2:])
+				thread[1] <-  struct{}{}	
+			case <-thread[2]:
+				testtasks.Wait(3,a[2:])
+				thread[2] <-  struct{}{}
+			case <-thread[3]:
+				testtasks.Wait(4,a[2:])
+				thread[3] <-  struct{}{}	
+			case <-thread[4]:
+				testtasks.Wait(5,a[2:])
+				thread[4] <-  struct{}{}	
+			}
 			<- channel2
-		}(wg)
+			
+		}(wg,a)
 	}
 	wg.Wait()
-	
-
 }
--- internal/testtasks/main.go
+++ internal/testtasks/main.go
@@ -4,30 +4,37 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
-	// "sync"
 	"time"
 )
-func Wait(args []string){
-	if len(args) == 3 && args[1]<args[2]{
-		
-		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "thread ",": starting job",args[0])
+func Wait(thread int,args []string){
+	if len(args) == 3{
 		min_time,err := strconv.Atoi(args[1])
-		if err != nil {
-			panic(err)
-		}
+			if err != nil {
+				fmt.Println("Ошибка! Неверные аргументы функции, 2 аргумент должен быть числом")
+				return
+			}
 		max_time,err := strconv.Atoi(args[2])
-		if err != nil {
-			panic(err)
+			if err != nil {
+				fmt.Println("Ошибка! Неверные аргументы функции, 3 аргумент должен быть числом")
+				return
+			}
+		if min_time < max_time{
+			fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "Thread #",thread,": starting job\"",args[0],"\"")
+			dalay_time := rand.Intn( max_time-min_time+1) + min_time
+			fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "Thread #",thread,": wating for", dalay_time,"seconds...")
+			time.Sleep(time.Duration(dalay_time)*time.Second)
+			fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "Thread #",thread,":job is complited")
+		} else {
+			fmt.Println("Ошибка! Неверные аргументы функции, минимальное время выполнения функции превосходит максимальное")
+			// Обработка передачи неверных аргументов функции, выбор способа реакции зависит от цели, предпологаю, что программа должна продолжить выполнение
+			// panic("Неверные аргументы функции..")
+			return
 		}
-		dalay_time := rand.Intn( max_time-min_time+1) + min_time
-		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "thread ",": wating for ", dalay_time)
-		time.Sleep(time.Duration(dalay_time)*time.Second)
-		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "thread ",":job is complited")
-		
+	} else{
+		fmt.Println("Ошибка! Неверные аргументы функции, ожидается 3 аргумента, получено", len(args))
+		// Обработка передачи неверных аргументов функции, выбор способа реакции зависит от цели, предпологаю, что программа должна продолжить выполнение
+		// panic("Неверные аргументы функции..")
+		return
 
-	} else {
-		fmt.Println("Неверные аргументы функции")
-// Обработка передачи неверных аргументов функции, выбор способа реакции зависит от цели
-		// panic("Неверные аргументы функции")
 	}
 }
